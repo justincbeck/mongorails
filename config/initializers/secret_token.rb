d8313d0f315e47b221cecac588eeb480e0b6636d
Justin Beck <justin.beck@meddius.com>
2011-06-01 11:07:17 -0400

    Initial check in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongorails::Application.config.secret_token = '77dc26ab313ff94fd4da9ea9cfbdf45d7f2bec1ee3eaab33c3a634d96fb531c0da9e90210cc77ebdadeee14466269f8471bab20e2ee2b2d9a3adea07b45aa9b7'
